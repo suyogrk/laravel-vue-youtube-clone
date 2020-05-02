2f9f31fcfb7eead6537722221e8a3b7a3a52979b
SuyogRK <suyong59@gmail.com>
2020-05-02 22:54:13 +0545

    use spatie medialibrary to upload image

--- app/Channel.php
+++ app/Channel.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use App\Model as Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class Channel extends Model
+class Channel extends Model implements HasMedia
 {
+    use HasMediaTrait;
     protected $fillable=['name'];
     //
 
--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -41,21 +41,22 @@ class ChannelController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Channel  $channel
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show(Channel $channel)
     {
         //
+        return view('channels.show', compact('channel'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Channel  $channel
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Channel $channel)
+    public function edit($id)
     {
         //
     }
@@ -64,21 +65,26 @@ class ChannelController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Channel  $channel
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Channel $channel)
     {
         //
+        if($request->hasFile('image')){
+            $channel->clearMediaCollection('images');
+            $channel->addMediaFromRequest('image')
+                ->toMediaCollection('images');
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Channel  $channel
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Channel $channel)
+    public function destroy($id)
     {
         //
     }
