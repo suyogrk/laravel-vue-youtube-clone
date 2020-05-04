e7ba2ac89c0e9b55086bd8e9b9e270a1e1655adf
SuyogRK <suyong59@gmail.com>
2020-05-04 10:32:37 +0545

    video upload

--- resources/views/channels/upload.blade.php
+++ resources/views/channels/upload.blade.php
@@ -1,61 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row justify-content-center">
-            <channel-uploads :channel="{{$channel}}" inline-template>
+            <channel-uploads :channel="{{ $channel }}" inline-template>
                 <div class="col-md-8">
+                    <div class="card p-3 d-flex justify-content-center align-items-center" v-if="!selected">
+                        <svg onclick="document.getElementById('video-files').click()" width="70px" height="70px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 461 461"><path d="M365.26 67.4H95.74A95.74 95.74 0 0 0 0 163.13v134.73a95.74 95.74 0 0 0 95.74 95.74h269.52A95.74 95.74 0 0 0 461 297.87V163.14a95.74 95.74 0 0 0-95.74-95.75zM300.5 237.05l-126.06 60.12a5.06 5.06 0 0 1-7.24-4.57v-124a5.06 5.06 0 0 1 7.34-4.52l126.06 63.88a5.06 5.06 0 0 1-.1 9.09z" fill="#f61c0d"/></svg>
 
-                    <div
-                        class="card p-3 d-flex justify-content-center align-items-center"
-                    v-if="!selected">
-                        <div onclick="document.getElementById('video-files').click()">Click to upload</div>
-                        <input
-                            type="file"
-                            multiple
-                            id='video-files'
-                            style="display:none"
-                            ref="videos"
-                            @change="upload"
-                        >
+                        <input type="file" multiple id="video-files" style="display: none;" ref="videos" @change="upload">
+                        <p class="text-center">Upload Videos</p>
                     </div>
+
                     <div class="card p-3" v-else>
-                        <div class="my-4">
+                        <div class="my-4" v-for="video in videos">
                             <div class="progress mb-3">
-                                <div
-                                    class="progress-bar progress-bar-striped progress-bar-animated"
-                                    style="width:50%"
-                                    role="progressbar"
-                                    aria-valuenow="50"
-                                    aria-valuemin="0"
-                                    aria-valuemax="100"
-                                >
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="d-flex justify-content-center align-items-center"
-                                        style="height: 100px; color:white; font-size: 10px; background: #808080">
-                                            Loading Thumbnail...
+                                <div class="progress-bar progress-bar-striped progress-bar-animated " role="progressbar" :style="{ width: `${progress[video.name]}%` }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div class="row">
 
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <h4 class="text-center">
-                                            My awesome videos
-                                        </h4>
+                                <div class="col-md-4">
+                                    <div class="d-flex justify-content-center align-items-center" style="height: 180px; color: white; font-size: 18px; background: #808080;">
+                                        Loading thumbnail ...
                                     </div>
                                 </div>
 
+                                <div class="col-md-4">
+                                    <h4 class="text-center">
+                                        @{{ video.name }}
+                                    </h4>
 
+
+                                </div>
                             </div>
                         </div>
-
-
                     </div>
-
                 </div>
             </channel-uploads>
-
         </div>
     </div>
 @endsection
